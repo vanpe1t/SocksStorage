58ea61d4874b6f4cd731d5bf91cc815275b74229
vanpe1t <makarov.maks.v@yandex.ru>
2023-03-28 14:33:51 +0300

    Initial commit

---
+++ src/main/java/ru/makarov/socksstorage/SocksStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.makarov.socksstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksStorageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/makarov/socksstorage/model/Socks.java
@@ -0,0 +1,2 @@
+package ru.makarov.socksstorage.model;public class Socks {
+}
